13684390ed9a525c359d77c5906cd243495207b8
oldbelom <oldbelom@gmail.com>
2019-12-24 21:45:38 +0300

    2nd screen && slider

--- app/js/main.js
+++ app/js/main.js
@@ -13,5 +13,12 @@ $(function(){
     slidesToScroll: 4,
     asNavFor: '.header__slider',
   })
+
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="left">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="right">',
+  });
   
 });
